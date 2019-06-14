3b7a5971508919b90521424f0d66eb4433b766ce
linfeng2078 <395208782@qq.com>
2019-06-14 16:08:38 +0800

    11

--- ZSZ.AdminWeb/Global.asax.cs
+++ ZSZ.AdminWeb/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using ZSZ.AdminWeb.App_Start;
+using ZSZ.Common;
 
 namespace ZSZ.AdminWeb
 {
@@ -13,6 +14,7 @@ namespace ZSZ.AdminWeb
         protected void Application_Start()
         {
             log4net.Config.XmlConfigurator.Configure();
+            ModelBinders.Binders.Add(typeof(string), new TrimToDBCModelBinder());
             GlobalFilters.Filters.Add(new ZSZExceptionFilter());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- ZSZ.FrontWeb/Global.asax.cs
+++ ZSZ.FrontWeb/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using ZSZ.Common;
 using ZSZ.FrontWeb.App_Start;
 
 namespace ZSZ.FrontWeb
@@ -13,6 +14,7 @@ namespace ZSZ.FrontWeb
         protected void Application_Start()
         {
             log4net.Config.XmlConfigurator.Configure();
+            ModelBinders.Binders.Add(typeof(string), new TrimToDBCModelBinder());
             GlobalFilters.Filters.Add(new ZSZExceptionFilter());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
